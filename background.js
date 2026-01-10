281c222d2c77221ecb8cfd8220a8adfc097a7d76
jinbago <info@thinkgj.com>
2026-01-11 00:06:49 +0900

    右クリック改善

--- background.js
+++ background.js
@@ -1,15 +1,3 @@
-function convertText(text) {
-  const zeroWidthSpace = '\u200B';
-  
-  let result = text;
-  
-  result = result.replace(/\n/g, zeroWidthSpace + '\n' + zeroWidthSpace);
-  
-  result = result.replace(/  /g, ' ' + zeroWidthSpace + ' ');
-  
-  return result;
-}
-
 chrome.runtime.onInstalled.addListener(() => {
   chrome.contextMenus.create({
     id: 'copy-with-whitespace',
@@ -19,17 +7,28 @@ chrome.runtime.onInstalled.addListener(() => {
 });
 
 chrome.contextMenus.onClicked.addListener((info, tab) => {
-  if (info.menuItemId === 'copy-with-whitespace' && info.selectionText) {
-    const converted = convertText(info.selectionText);
-    
+  if (info.menuItemId === 'copy-with-whitespace') {
     chrome.scripting.executeScript({
       target: { tabId: tab.id },
-      func: (text) => {
-        navigator.clipboard.writeText(text).then(() => {
-          window.postMessage({ type: 'COPY_SUCCESS' }, '*');
-        });
-      },
-      args: [converted]
+      func: () => {
+        const convertText = (text) => {
+          const zeroWidthSpace = '\u200B';
+          let result = text;
+          result = result.replace(/\n/g, zeroWidthSpace + '\n' + zeroWidthSpace);
+          result = result.replace(/  /g, ' ' + zeroWidthSpace + ' ');
+          return result;
+        };
+        
+        const selection = window.getSelection();
+        const text = selection.toString();
+        
+        if (text) {
+          const converted = convertText(text);
+          navigator.clipboard.writeText(converted).then(() => {
+            window.postMessage({ type: 'COPY_SUCCESS' }, '*');
+          });
+        }
+      }
     });
   }
 });
